60513c0496835002e9c7f7dbf0342d7f2fdb0672
Dinor Geler <digeler@users.noreply.github.com>
2018-10-07 19:00:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 !/bin/bash
-cp /home/file/cs /lib/systemd/system/gazure.service
-cp /home/file/cs/server /usr/local/bin/
+cp -rf /home/file/cs /lib/systemd/system/gazure.service
+cp -rf /home/file/cs/server /usr/local/bin/
 systemctl enable gazure
 systemctl start gazure
