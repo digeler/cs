a61da963263855b5e65d2a1be57d68df1c0ceb2f
Dinor Geler <digeler@users.noreply.github.com>
2018-10-08 11:09:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-WORKING_DIR=/home/file
-if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
 cp -rf /home/file/cs/cs /lib/systemd/system/gazure.service
 cp -rf /home/file/cs/cs/server /usr/local/bin/
 systemctl enable gazure
